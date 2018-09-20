83dd60ea8bfb0062fdf3cd402e25769eb499a68d
Christopher Beltran <christod.beltran@gmail.com>
2018-09-20 05:19:39 +0000

    Fixed player name loop to only pull top 20 players

--- lib/fantasy/projections/scraper.rb
+++ lib/fantasy/projections/scraper.rb
@@ -12,15 +12,11 @@ class Scraper
 def self.qb_scraper
   doc = Nokogiri::HTML(open("https://www.fantasypros.com/nfl/projections/qb.php"))
   player_label = doc.css(".player-label")
-player_label.css(".player-name").each do |player_name|
-    counter = 0
-until counter == 20
-  qb_hash = {
+player_label.css(".player-name").take(20).each do |player_name|
+    qb_hash = {
     :name => player_name.text
   }
-  counter += 1
   @@all << qb_hash
-end
 end
 @@all
 binding.pry
