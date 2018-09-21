389715ed5b5e4caa5480097c1a0ddeacc425155f
Christopher Beltran <christod.beltran@gmail.com>
2018-09-21 06:21:40 +0000

    Implemented qb projected points scraper(currently listing the first players projected points for each scraped QB, needs fixing)

--- lib/fantasy/projections/scraper.rb
+++ lib/fantasy/projections/scraper.rb
@@ -10,17 +10,27 @@ class Scraper
   end 
   
 def self.qb_scraper
-  doc = Nokogiri::HTML(open("https://www.fantasypros.com/nfl/projections/qb.php"))
-  player_label = doc.css(".player-label")
+  @doc = Nokogiri::HTML(open("https://www.fantasypros.com/nfl/projections/qb.php"))
+  player_label = @doc.css(".player-label")
 player_label.css(".player-name").take(20).each do |player_name|
-    qb_hash = {
+    @qb_hash = {
     :name => player_name.text
   }
-  @@all << qb_hash
+  @@all << @qb_hash
 end
-@@all
-binding.pry
-end 
 end
 
+def self.qb_points_scraper
+  x = 14
+  if x <= 223
+  proj = @doc.css("td")[x].text
+  @@all.each do |player|
+    player[:projection] = proj
+  x += 11
+end 
+end 
+end
+end 
 Scraper.qb_scraper
+Scraper.qb_points_scraper
+binding.pry
