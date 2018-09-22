e3678ab7d1bf1c1ada5abcc066e2bd2f0a5c10cd
Christopher Beltran <christod.beltran@gmail.com>
2018-09-22 21:21:32 +0000

    Added rb_scraper and rb_points scraper, need to fix points scraper iteration

--- lib/fantasy/projections/scraper.rb
+++ lib/fantasy/projections/scraper.rb
@@ -3,6 +3,7 @@ require 'nokogiri'
 require 'open-uri'
 
 class Scraper
+  attr_accessor :qb_hash, :rb_hash
   
 @@all = []
   def self.all
@@ -26,8 +27,28 @@ def self.qb_points_scraper
     player[:projection] = @doc.css("td")[proj].text
   end 
 end
+
+def self.rb_scraper
+  @doc = Nokogiri::HTML(open("https://www.fantasypros.com/nfl/projections/rb.php"))
+  player_label = @doc.css(".player-label")
+player_label.css(".player-name").take(30).each do |player_name|
+    @rb_hash = {
+    :name => player_name.text
+  }
+  @@all << @rb_hash
+end
 end
 
+def self.rb_points_scraper
+  points_columns = [12,21,30,39,48,57,66,75,84,93,102,111,120,129,138,147,156,165,174,183,192,201,210,219,228,237,246,255,264,273]
+  @@all.zip(points_columns).each do |player, proj|
+    player[:projection] = @doc.css("td")[proj].text
+  end 
+end
+end
+end
 Scraper.qb_scraper
 Scraper.qb_points_scraper
+Scraper.rb_scraper
+Scraper.rb_points_scraper
 binding.pry
