6418f284e47baded86cde089b1aacb028ce5b0f7
Christopher Beltran <christod.beltran@gmail.com>
2018-09-23 02:38:16 +0000

    Fixed doc variables

--- lib/fantasy/projections/scraper.rb
+++ lib/fantasy/projections/scraper.rb
@@ -15,11 +15,13 @@ class Scraper
     self.wr_points_scraper
     self.te_scraper
     self.te_points_scraper
+    
+    @@all
   end 
   
   def self.qb_scraper
-    @doc = Nokogiri::HTML(open("https://www.fantasypros.com/nfl/projections/qb.php"))
-    player_label = @doc.css(".player-label")
+    @qb_doc = Nokogiri::HTML(open("https://www.fantasypros.com/nfl/projections/qb.php"))
+    player_label = @qb_doc.css(".player-label")
     player_label.css(".player-name").take(20).each do |player_name|
       @qb_hash = {
       :name => player_name.text
@@ -31,13 +33,13 @@ class Scraper
   def self.qb_points_scraper
   points_columns = [14,25,36,47,58,69,80,91,102,113,124,135,146,157,168,179,190,201,212,223]
     @@all.zip(points_columns).each do |player, proj|
-      player[:projection] = @doc.css("td")[proj].text
+      player[:projection] = @qb_doc.css("td")[proj].text
     end 
   end
 
   def self.rb_scraper
-  @doc = Nokogiri::HTML(open("https://www.fantasypros.com/nfl/projections/rb.php"))
-    player_label = @doc.css(".player-label")
+  @rb_doc = Nokogiri::HTML(open("https://www.fantasypros.com/nfl/projections/rb.php"))
+    player_label = @rb_doc.css(".player-label")
     player_label.css(".player-name").take(30).each do |player_name|
     @rb_hash = {
     :name => player_name.text
@@ -49,13 +51,13 @@ class Scraper
   def self.rb_points_scraper
   points_columns = [12,21,30,39,48,57,66,75,84,93,102,111,120,129,138,147,156,165,174,183,192,201,210,219,228,237,246,255,264,273]
      @@all[20..49].zip(points_columns).each do |player, proj|
-       player[:projection] = @doc.css("td")[proj].text
+       player[:projection] = @rb_doc.css("td")[proj].text
        end 
   end
   
   def self.wr_scraper
-  @doc = Nokogiri::HTML(open("https://www.fantasypros.com/nfl/projections/wr.php"))
-    player_label = @doc.css(".player-label")
+  @wr_doc = Nokogiri::HTML(open("https://www.fantasypros.com/nfl/projections/wr.php"))
+    player_label = @wr_doc.css(".player-label")
     player_label.css(".player-name").take(30).each do |player_name|
     @wr_hash = {
     :name => player_name.text
@@ -67,13 +69,13 @@ class Scraper
   def self.wr_points_scraper
   points_columns = [12,21,30,39,48,57,66,75,84,93,102,111,120,129,138,147,156,165,174,183,192,201,210,219,228,237,246,255,264,273]
      @@all[50..79].zip(points_columns).each do |player, proj|
-       player[:projection] = @doc.css("td")[proj].text
+       player[:projection] = @wr_doc.css("td")[proj].text
        end 
   end
   
   def self.te_scraper
-  @doc = Nokogiri::HTML(open("https://www.fantasypros.com/nfl/projections/te.php"))
-    player_label = @doc.css(".player-label")
+  @te_doc = Nokogiri::HTML(open("https://www.fantasypros.com/nfl/projections/te.php"))
+    player_label = @te_doc.css(".player-label")
     player_label.css(".player-name").take(15).each do |player_name|
     @te_hash = {
     :name => player_name.text
@@ -85,7 +87,7 @@ class Scraper
   def self.te_points_scraper
   points_columns = [8,14,20,26,32,38,44,50,56,62,68,74,80,86,92]
      @@all[80..94].zip(points_columns).each do |player, proj|
-       player[:projection] = @doc.css("td")[proj].text
+       player[:projection] = @te_doc.css("td")[proj].text
        end 
   end
   
@@ -108,4 +110,4 @@ class Scraper
     @profile_hash
   end 
   end 
-  
+  binding.pry
